5e9042a3cb5bb7887db94ae137288102902b2e6c
Laura Topacio Valdez Morones <lauratopaciovaldez@gmail.com>
2019-03-12 09:51:13 -0700

    AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\Schema;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
+    
     /**
      * Bootstrap any application services.
      *
